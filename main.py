b3b058051aa9f2233e1d72346676bf84af79bc0b
Furanin <7533248@gmail.com>
2024-05-25 03:23:25 +0300

    edit main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from src.processing import list_dictionary, list_sorted_date
 from src.widget import convert_date, get_masks_bank_accounts_cards
 
 card_check_number = str(input("Введите номер карты или счет: "))
@@ -5,3 +6,25 @@ print(get_masks_bank_accounts_cards(card_check_number))
 
 date_number = str(input("Введите строку даты: "))
 print(convert_date(date_number))
+
+print(
+    list_dictionary(
+        [
+            {"id": "41428829", "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": "939719570", "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": "594226727", "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": "615064591", "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
+
+print(
+    list_sorted_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
